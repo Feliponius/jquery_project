133c1c9889bf270cdc721fd862da9ff5f04e236f
Feliponius <PhilipPixler841@gmail.com>
2015-06-08 21:44:14 -0500

    Big update
    Added background
    Removed Tiles
    Added sizing for the browser window (no scrolling, not mobile friendly.)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 /*function that executes on startup*/
 $(document).ready(function() {
+	
+	//CSS for button//
+	
 	var button = {
 		display: 'block',
 		margin: '5 auto 2',
@@ -10,41 +13,69 @@ $(document).ready(function() {
 		border: '1px solid #ddd',
 		lineheight: 30
 	}
-
+	
+	//Element Attributes for button//
+	
 	var buttonAtt = {
 		id: 'resetButton',
 		value: 'Reset Grid'
 	}
 
 
-	$('<input />').attr('type', 'button').attr(buttonAtt).css(button).appendTo('.Wrapper');
+	$('<input />').attr('type', 'button').attr(buttonAtt).css(button).prependTo('body');
 	
-	for(var i = 1; i<=256; i++){
-   		$('<div />').addClass('sameDiv').attr('id','div'+i).appendTo('.wrapper');
+	//startup//
+
+	var size = (1080/16);
+
+	math = Math.floor(938/size);
+
+	loop = 16 * math + .5;
+
+	for(var i = 1; i < loop; i++){
+   		$('<div />').addClass('sameDiv').attr('id','div'+i).css({
+   			width: size,
+   			height: size,
+   			background: '#ffffff',
+   		}).appendTo('.wrapper');
 	}
+
+	//startup hover effect//
+
+	$('.sameDiv').hover(
+		function() {
+			$(this).css('background-color', 'blue')
+		})
+
+	//reset function//
 
 	$('#resetButton').click(function() {
 		var sides = prompt('How many sides would you like the grid to have?');
 
 		$('.sameDiv').remove();
 
-		var size = (960)/sides-2;
+		var size = (1080)/sides;
 
-		for (var i=1; i <= sides*sides; i++) {
-			$('<div />').addClass('sameDiv').attr('id','div'+i).css({width: size, height: size}).appendTo('.wrapper');
+		math = Math.floor(938/size);
+
+		loop = sides * math + .5;
+
+		for (var i=1; i <= loop; i++) {
+			$('<div />').addClass('sameDiv').attr('id','div'+i).css({
+				width: size, 
+				height: size,
+				background: '#ffffff',
+	   			//margin: '1px',
+	   			//'border-radius': '2px'
+	   			//border: '1px solid #666'
+			}).appendTo('.wrapper');
 		}
-
+			//function hover effect//
 			$('.sameDiv').hover(
-		function() {
-			$(this).css('background-color', 'blue')
-		})
-
+				function() {
+					$(this).css('background-color', 'blue')
+				})
 	})
-
-	$('.sameDiv').hover(
-		function() {
-			$(this).css('background-color', 'blue')
-		})
 })
 
 
